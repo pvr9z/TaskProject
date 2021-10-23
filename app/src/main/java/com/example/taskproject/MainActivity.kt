c36579a779b7480dc9bf9843bfa53ed13f016294
Pvr01 <pvr.ram569@gmail.com>
2021-10-23 18:20:01 +0530

    optimized

--- app/src/main/java/com/example/taskproject/MainActivity.kt
+++ app/src/main/java/com/example/taskproject/MainActivity.kt
@@ -2,11 +2,6 @@ package com.example.taskproject
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
-import androidx.navigation.NavController
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.taskproject.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
